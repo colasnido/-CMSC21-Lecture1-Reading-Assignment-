f95cead0f4645a299d8eb7d1629813f927ed5a9c
Nicole Amoguis <125950565+colasnido@users.noreply.github.com>
2023-02-27 22:33:04 +0800

    Page 38 – ch3_ex1.c

---
+++ Page 38 – ch3_ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int i, j;
+float x, y;
+
+/* initializing the variables with their corresponding value */
+i = 10;
+j = 20;
+x = 43.2892;
+y = 5527.0;
+
+/* printing the variables */
+printf("i = %d, j = %d, x = %f, y = %f \n", i, j, x, y);
+
+}
